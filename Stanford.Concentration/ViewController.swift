5940fcb05f678cbd1f1942ed36d6ece0339fddd1
Razumov Pavel <i@dizzyfly.ru>
2022-07-27 13:33:11 +0300

    Initial Commit

---
+++ Stanford.Concentration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stanford.Concentration
+//
+//  Created by Pavel Razumov on 27.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
