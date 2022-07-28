10b90db37eb99a6279a6c798d68f70464956612d
Razumov Pavel <i@dizzyfly.ru>
2022-07-28 18:49:57 +0300

    Создал модель карты и модель игры

---
+++ Stanford.Concentration/ConcentrationGame.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationGame.swift
+//  Stanford.Concentration
+//
+//  Created by Pavel Razumov on 28.07.2022.
+//
+
+import Foundation
+
+class ConcentrationGame {
+    
+    var cards = [Card]()
+    
+    func chooseCard(at index: Int) {
+        
+    }
+}
